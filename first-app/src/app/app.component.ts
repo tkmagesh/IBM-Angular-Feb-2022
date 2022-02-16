58284796624bfeebad4af97cb642006434c64f92
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-16 12:29:52 +0530

    menu - assignment - solution

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular App';
 
+  userChoice : string = '';
+  
   constructor(){
    /*  setTimeout(() => {
       this.title = 'My-New-App'
