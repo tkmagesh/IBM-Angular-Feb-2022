271e1724d724d52d88662d722016f03814f90c92
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-16 17:04:24 +0530

    Use case 7 : improvise bug display (creating custom pipes)

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BugsComponent } from './bugs/bugs.component';
+import { TrimTextPipe } from './bugs/pipes/trim-text.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BugsComponent
+    BugsComponent,
+    TrimTextPipe
   ],
   imports: [
     BrowserModule
---
+++ bug-tracker-app/src/app/bugs/pipes/trim-text.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TrimTextPipe } from './trim-text.pipe';
+
+describe('TrimTextPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TrimTextPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
