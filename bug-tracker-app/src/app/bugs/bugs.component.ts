88459106f9d21ff55a4592c4d38f6cbf7215da14
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-16 16:40:49 +0530

    Use cases 4,5, & 6 implementation

--- bug-tracker-app/src/app/bugs/bugs.component.ts
+++ bug-tracker-app/src/app/bugs/bugs.component.ts
@@ -32,4 +32,48 @@ export class BugsComponent implements OnInit {
     this.bugs.splice(this.bugs.indexOf(bugToRemove), 1)
   }
 
+  onBugNameClick(bugToToggle : Bug){
+    bugToToggle.isClosed = !bugToToggle.isClosed;
+  }
+
+  onRemoveClosedClick(){
+    /* approach-1 */
+    /* 
+    for (let idx = this.bugs.length-1; idx >=0; idx--){
+      let bug = this.bugs[idx];
+      if (bug.isClosed){
+        this.bugs.splice(idx, 1)
+      }
+    } 
+    */
+
+    /* approach-2 */
+    /* 
+    this.bugs = this.bugs.filter(function(bug){
+      return !bug.isClosed;
+    }); 
+    */
+
+    this.bugs = this.bugs.filter(bug => !bug.isClosed);
+
+  }
+
+  /* TOBE : Fixed  */
+  getClosedCount() : number {
+    /* Approach-1 */
+    /* 
+    let closedCount = 0;
+    for (let bug of this.bugs){
+      if (bug.isClosed)
+        ++closedCount
+    }
+    return closedCount 
+    */
+
+    /* Approach-2 */
+    /* return this.bugs.filter(bug => bug.isClosed).length */
+    
+    /* Approach-3 */
+    return this.bugs.reduce((result, bug) => bug.isClosed ? result + 1 : result, 0)
+  }
 }
