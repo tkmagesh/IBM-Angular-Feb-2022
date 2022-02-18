51885d43a1b36ef88e8ec6341d27eb26e867ccaf
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-18 10:26:29 +0530

    async using callbacks

--- 02-async-demos/demos.js
+++ 02-async-demos/demos.js
@@ -16,20 +16,21 @@
 
     window['addSyncClient'] = addSyncClient;
 
-    //Async
-    function adddAsync(x,y){
+    //Async (using callbacks)
+    function adddAsync(x,y, callback){
         console.log(`   [@service] processing ${x} and ${y}`)
         setTimeout(function(){
             const result = x + y;
             console.log(`   [@service] returning result`);
-            return result;
+            callback(result);
         },5000);
     }
 
     function adddAsyncClient(x,y){
         console.log(`[@client] invoking the service`);
-        const result = adddAsync(x,y);
-        console.log(`[@client] result = ${result}`)
+        adddAsync(x,y, function(result){
+            console.log(`[@client] result = ${result}`)
+        });
     }
 
     window['adddAsyncClient'] = adddAsyncClient;
